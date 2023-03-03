9a19a5cf81708418c812bdd0a4c71131c2237d24
lance <oplancelot@gmail.com>
2023-03-03 15:13:50 +0800

    flag

---
+++ practice/command/Retrieving/Retrieving.go
@@ -0,0 +1,49 @@
+// flag.[Type]Var()
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+// var errRE = regexp.MustCompile(`(?i)error`)
+
+var errRE = regexp.MustCompile(`(?i)123`)
+
+func main() {
+	var s *bufio.Scanner
+	switch len(os.Args) {
+	case 1:
+		log.Println("No file specified, using STDIN")
+		s = bufio.NewScanner(os.Stdin)
+	case 2:
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		s = bufio.NewScanner(f)
+	default:
+		log.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	for s.Scan() {
+		line := s.Bytes()
+		if errRE.Match(line) {
+			fmt.Printf("%s\n", line)
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Println("Error: ", err)
+		os.Exit(1)
+	}
+}
+
+// [root@tx01 ~/Go-for-DevOps/practice/command/Retrieving]# go run Retrieving.go ./error.md
+// error
+//但是var errRE = regexp.MustCompile(`(?i)123`)
+// [root@tx01 ~/Go-for-DevOps/practice/command/Retrieving]# go run Retrieving.go ./error.md
+// 123
